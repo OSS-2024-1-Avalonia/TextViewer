3441fbd210947a11a465617dd084801b8a60d0a1
xellkorn <xellkorn@gmail.com>
2024-05-11 23:49:46 +0900

    [UI] 프로그램 디자인 구현

---
+++ TextViewer/MainWindow.axaml.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace TextViewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+        }
+
+        private async void SaveButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
